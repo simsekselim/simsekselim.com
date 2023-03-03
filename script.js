37896b76e2f0fd4dfac3f936f99a58a9e7dbe066
simsekselim <simsekselim06@gmail.com>
2023-03-03 17:54:46 +0300

    renewed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('Its working')
+let projects = [];
 
 let theme = localStorage.getItem('theme')
 
@@ -19,6 +19,16 @@ for (var i=0; themeDots.length > i; i++){
 	})
 }
 
+fetch('https://codexall-7ec9f-default-rtdb.firebaseio.com/projects.json')
+  .then(response => response.json())
+  .then(data => {
+    projects = data;
+    renderProjects();
+  });
+
+
+
+
 function setTheme(mode){
 	if(mode == 'light'){
 		document.getElementById('theme-style').href = 'default.css'
@@ -38,4 +48,54 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
+
+
+function renderProjects() {
+  const slider = document.querySelector('.post-slider');
+
+  // Clear any existing slides
+  slider.innerHTML = '';
+
+  // Loop through the projects data and create a slide for each project
+  projects.forEach(project => {
+    const slide = document.createElement('div');
+    slide.classList.add('slide');
+
+    const post = document.createElement('div');
+    post.classList.add('post');
+
+    const thumbnail = document.createElement('img');
+    thumbnail.classList.add('thumbnail');
+    thumbnail.setAttribute('src', project.image);
+    post.appendChild(thumbnail);
+
+    const postPreview = document.createElement('div');
+    postPreview.classList.add('post-preview');
+
+    const postTitle = document.createElement('h6');
+    postTitle.classList.add('post-title');
+    postTitle.textContent = project.title;
+    postPreview.appendChild(postTitle);
+
+    const postIntro = document.createElement('p');
+    postIntro.classList.add('post-intro');
+    postIntro.textContent = project.description;
+    postPreview.appendChild(postIntro);
+
+    const playNowLink = document.createElement('a');
+	playNowLink.setAttribute('target', '_blank')
+    playNowLink.setAttribute('href', project.link);
+    if(project.title == "Fodamy"){
+      playNowLink.textContent = 'Check Out';
+    }else{
+      playNowLink.textContent = 'Play Now';
+    }
+    postPreview.appendChild(playNowLink);
+
+    post.appendChild(postPreview);
+    slide.appendChild(post);
+    slider.appendChild(slide);
+  });
+}
+
 
